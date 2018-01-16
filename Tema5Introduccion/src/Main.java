4a6ad8b8c14e89f17a495c1313862970fdadb034
Alejandro <wateralexpolo@gmail.com>
2018-01-16 16:30:53 +0100

    hi

---
+++ Tema5Introduccion/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Persona p = new Persona();
+		
+		p.apellidos = "Pérez López";
+		p.nombre = "Laura";
+		p.dni = "43212342F";
+		
+		p.show();
+		
+		Persona p2 = new Persona();
+		
+		p2.apellidos = "Martínez Valls";
+		p2.nombre = "Manolo";
+		p2.dni = "32131232H";
+		
+		p2.show();
+		
+		
+	}
+
+}
---
+++ Tema5Introduccion/src/Persona.java
@@ -0,0 +1,16 @@
+
+public class Persona {
+	
+	// Atributos de la clase.
+	String apellidos;
+	String nombre;
+	String dni;
+	
+	// Métodos de la clase.
+	
+	void show(){
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellidos);
+		System.out.println("DNI: " + dni);	}
+	
+}
